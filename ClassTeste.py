fb6df475ad33afb382f5c36eb09282557fa97049
Grecco <lucasgrecco@gmail.com>
2017-06-21 12:45:01 -0300

    Refacting ClassTeste

--- ClassTeste.py
+++ ClassTeste.py
@@ -14,23 +14,3 @@ class ClassTeste:
             print(getattr(self, attr_string))
         else:
             print("Atributo não definido.")
-
-
-teste = ClassTeste("Lucas", "30 anos")
-teste.print_teste()
-
-teste.nome = "Lucas Grecco"
-print(teste.nome)
-print(hasattr(teste, "nome"))
-print(hasattr(teste, "idade"))
-print(hasattr(teste, "endereco"))
-#setando atributo nao definido explicitamente
-setattr(teste, "endereco", "aclimacao, peixoto oliveira.")#mesmo nao existindo o atributo, vc pode definir. Atua como uma classe generica
-teste.print_attr("endereco")
-# print(hasattr(teste, "endereco"))
-print(teste.endereco)
-delattr(teste, "endereco")
-teste.print_attr("endereco")
-
-
-
--- teste_classes.py
+++ teste_classes.py
@@ -1,5 +1,22 @@
-import ClassTeste
+from ClassTesteAdvc import ClassTeste
 
-teste = ClassTeste('lucas', '30')
+teste = ClassTeste("Lucas", "30")
 ClassTeste.print_teste(teste)
 print(teste)
+
+
+teste = ClassTeste("Lucas", "30 anos")
+teste.print_teste()
+
+teste.nome = "Lucas Grecco"
+print(teste.nome)
+print(hasattr(teste, "nome"))
+print(hasattr(teste, "idade"))
+print(hasattr(teste, "endereco"))
+#setando atributo nao definido explicitamente
+setattr(teste, "endereco", "aclimacao, peixoto oliveira.")#mesmo nao existindo o atributo, vc pode definir. Atua como uma classe generica
+teste.print_attr("endereco")
+# print(hasattr(teste, "endereco"))
+print(teste.endereco)
+delattr(teste, "endereco")
+teste.print_attr("endereco")
